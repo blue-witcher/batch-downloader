2264b597998a3a7883009a6fd162e7fb814d9781
blue-witcher <>
2024-12-01 18:08:00 +0100

    remove file_base_path since -O flag overrides it and does the same

--- recursive_downloader.fish
+++ recursive_downloader.fish
@@ -3,10 +3,9 @@ cat download_list.txt | read -za url
 set file_base booklet_
 read -P "What is the file base-name? " file_base
 read -P "Where should the counter start? " counter
-read -P "Where to put the files? " file_base_path
 read -P "File extension? " file_extension
 
 for i in $url
-    wget $i -P (echo $file_base_path) -O (echo $file_base\_$counter$file_extension) -q &
+    wget $i -O (echo $file_base\_$counter$file_extension) -q &
     set counter (math $counter + 1)
 end
